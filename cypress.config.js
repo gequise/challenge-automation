988923058894cacd4599d03a191b8fca96b1042e
gequise <gustavo.equise@gmail.com>
2023-04-12 03:30:45 -0300

    added allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
   e2e: {
@@ -6,6 +7,8 @@ module.exports = defineConfig({
     baseUrl: "https://www.saucedemo.com",
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
       // implement node event listeners here
     },
   },
